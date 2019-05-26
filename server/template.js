d71d59e7470164f27cb88c1208492474b3102265
Benjamin Mildren <mildrenben@gmail.com>
2019-05-26 15:48:08 +0100

    Added axis and proptypes. Make api call on server and pass on data to react app

--- server/template.js
+++ server/template.js
@@ -3,22 +3,27 @@ const ReactDOMServer = require("react-dom/server");
 const fs = require("fs");
 const path = require("path");
 const ReactApp = require("../src/App.js").default;
+const axios = require("axios");
 
-const Foo = () => (
-  <div>
-    <b>fghiugfiudughfhiu</b>
-  </div>
-);
-
-module.exports = res =>
-  fs.readFile(
-    path.join(__dirname + "/../build/index.html"),
-    "utf-8",
-    (err, data) => {
-      data.replace(
-        '<div id="root"></div>',
-        ReactDOMServer.renderToString(<ReactApp />)
+module.exports = res => {
+  // API data call (wasnt sure if this was meant to be server side or client side,
+  // if it was meant to be client side, it would be east enought to move to an onMount call
+  // and show a loader whilst the client waits for the data
+  axios
+    .get("https://swapi.co/api/people/1")
+    .then(starWarsResponse => starWarsResponse.data)
+    .then(starWarsData => {
+      // Renders the react code before sending
+      fs.readFile(
+        path.join(__dirname + "/../build/index.html"),
+        "utf-8",
+        (err, data) => {
+          data.replace(
+            '<div id="root"></div>',
+            ReactDOMServer.renderToString(<ReactApp data={starWarsData} />)
+          );
+          res.send(data);
+        }
       );
-      res.send(data);
-    }
-  );
+    });
+};

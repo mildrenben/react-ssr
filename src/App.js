0407cb8c5768a6df1486bfe38df818faf142a0b2
Benjamin Mildren <mildrenben@gmail.com>
2019-05-26 17:32:40 +0100

    Added react-helmet and google font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import Header from "./components/Header";
 import Character from "./components/Character";
 import "./App.css";
 
 const App = ({ data }) => (
   <div>
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css?family=Karla&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+    <Header />
     {data.results.map(character => (
       <Character {...character} />
     ))}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.hydrate(
   <App data={window.__INITIAL_DATA__} />,
